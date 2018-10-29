e0b9c01c932a0b5f71406a37b83f87ee63b0abba
YounSung <jys221010@gmail.com>
2018-10-29 06:25:05 +0000

    telegram

---
+++ telegram.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+token='667661067:AAHS-NSuJewUa3mZ9calLXPEQ4eIci1AWDc'
+method_name='getUpdates'
+url='https://api.telegram.org/bot{}/{}'.format(token,method_name)
+update=requests.get(url).json()
+
+#################
+kospi_url='https://finance.naver.com/sise/'
+html=requests.get(kospi_url).text
+soup=BeautifulSoup(html,'html.parser')
+select=soup.select_one('#KOSPI_now').text
+#print(update['result'][0]['message']['from']['id'])
+#################
+user_id=update['result'][0]['message']['from']['id']
+method_name='sendmessage'
+msg=select
+msg_url='https://api.telegram.org/bot{}/{}?chat_id={}&text={}'.format(token,method_name,user_id,msg)
+print(requests.get(msg_url))
